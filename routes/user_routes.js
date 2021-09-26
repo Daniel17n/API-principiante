8c5d922a8b19cef84e6a2eb572b50f0414482ce3
danie17n <daniel16nhotmail.com>
2021-09-26 13:07:44 +0200

    Añadido require

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
+const userController = require('./controllers/user_Controller');
+
 router.get('/users', user_Controller.getUsers);
 
 router.get('/db', user_Controller.getDB);
