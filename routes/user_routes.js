2315a358575203bb7aebda6f871e797c23d7398a
danie17n <daniel16nhotmail.com>
2021-09-26 13:17:39 +0200

    Resolución 1

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const userController = require('./controllers/user_Controller');
+const userController = require('../controllers/user_Controller');
 
 router.get('/users', user_Controller.getUsers);
 
