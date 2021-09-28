e3289e6282e86a3c1630c98a4e333ccf2e838d97
danie17n <daniel16nhotmail.com>
2021-09-28 10:50:59 +0200

    post añadido a medias

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const laptopRoutes = require('./routes/laptopRoutes');
+const laptopController = require('./controllers/laptopController');
 
 const app = express();
-
-const bodyParser = require('body-parser');
-
-const laptopRoutes = require('./routes/laptopRoutes');
-
 const port = process.env.PORT || 3000;
+const router = express.Router();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.send('Ponle algo más, aquí no hay nada!');
-});
-
+router.post('/', laptopController.addLaptop);
 app.use(router);
-
 app.use('/', laptopRoutes);
-
 app.listen(port, () => {
   console.log('Node server running on http://localhost:3000');
 });
--- routes/laptopRoutes.js
+++ routes/laptopRoutes.js
@@ -3,7 +3,7 @@ const laptopController = require('../controllers/laptopController');
 
 router.get('/model', laptopController.getModel);
 router.get('/db', laptopController.getDB);
-// router.post('/post', laptopController.createLaptop);
+router.post('/', laptopController.addLaptop);
 // router.post('/find', laptopController.findLaptop);
 
 module.exports = router;
